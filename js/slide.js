b706cc0fc0db5e7b1395eb13a2d7f1ba596636cc
fe2oz <93234748+fe2oz@users.noreply.github.com>
2022-01-19 13:12:18 +0900

    01.19/13:12

--- js/slide.js
+++ js/slide.js
@@ -1,19 +1,15 @@
 $(document).ready(function(){
-    var lis,
-        lisNum;
+    const lis,
+        lisNum,
         imgLis,
         imgLisNum
 
     lis = document.getElementById("snmt1");
-    lisNum = lis.children();
+    lisNum = lis.children;
     imgLis = document.getElementById("snmp1");
-    imgLisNum = imgLis.children();
+    imgLisNum = imgLis.children;
 
-    for(var i=0; i<lisNum.length; i++){
-        lisNum[i].addEventListener("click", function(){
-            for(var j=0; j<imgLisNum.length; j++){
-                imgLisNum[j].style.left = "-200px";
-            }
-        });
-    }
+    lisNum.addEventListener("click", function(){
+        alert(lisNum.index)
+    })
 });
