50e3d066b8e89bb3b166db9eca9d85b41ea3f0e9
fe2oz <93234748+fe2oz@users.noreply.github.com>
2022-01-24 21:44:30 +0900

    01.24/21:44

---
+++ js/sub/sub.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $.ajax({
+        method: "GET",
+        url: "https://dapi.kakao.com/v3/search/book?target=title",
+        data: {query:"도리언 그레이의 초상"},
+        headers: {Authorization: "KakaoAK 58f6d0b6aa8f0767623a8d35f4ef2f61"}
+      })
+        .done(function( msg ) {
+            console.log(msg);
+
+            
+
+        });
+
+        $(function () {
+          $.get("./test_txt/test.txt", function (data) {
+            $("#tmpBox").html(data);
+          })
+  
+        });
+});
+
+$(document).ready(function(){
+    $("#so1 li").click(function(){
+        $("#so1 li").removeClass("active");
+        $(this).addClass("active");
+    });
+
+
+})
