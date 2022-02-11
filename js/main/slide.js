2d1f7e51b4a975391b2f6df728f5a0eb506d9522
fe2oz <93234748+fe2oz@users.noreply.github.com>
2022-02-11 18:38:57 +0900

    02.11/18:38

--- js/main/slide.js
+++ js/main/slide.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
-    
+    var chars = $('#snmp1 li').width();
+
     $("#snmt1 li:nth-of-type(1)").click(function(){
         $("#snmp1").stop().animate({marginLeft: 0});
     });
 
     $("#snmt1 li:nth-of-type(2)").click(function(){
-        $("#snmp1").stop().animate({marginLeft: -274.97});
+        $("#snmp1").stop().animate({marginLeft: -chars});
     });
 
     $("#snmt1 li:nth-of-type(3)").click(function(){
-        $("#snmp1").stop().animate({marginLeft: -545.94});
+        $("#snmp1").stop().animate({marginLeft: -chars * 2});
     });
 
     $("#snmt1 li:nth-of-type(4)").click(function(){
-        $("#snmp1").stop().animate({marginLeft: -825.94});
+        $("#snmp1").stop().animate({marginLeft: -chars * 3});
     });
 
     $("#snmt2 li:nth-of-type(1)").click(function(){
@@ -21,15 +22,15 @@ $(document).ready(function(){
     });
 
     $("#snmt2 li:nth-of-type(2)").click(function(){
-        $("#snmp2").stop().animate({marginLeft: -274.97});
+        $("#snmp2").stop().animate({marginLeft: -chars});
     });
 
     $("#snmt2 li:nth-of-type(3)").click(function(){
-        $("#snmp2").stop().animate({marginLeft: -549});
+        $("#snmp2").stop().animate({marginLeft: -chars * 2});
     });
 
     $("#snmt2 li:nth-of-type(4)").click(function(){
-        $("#snmp2").stop().animate({marginLeft: -825.94});
+        $("#snmp2").stop().animate({marginLeft: -chars * 3});
     });
 
     $("#snmt3 li:nth-of-type(1)").click(function(){
@@ -37,11 +38,11 @@ $(document).ready(function(){
     });
 
     $("#snmt3 li:nth-of-type(2)").click(function(){
-        $("#snmp3").stop().animate({marginLeft: -274.97});
+        $("#snmp3").stop().animate({marginLeft: -chars});
     });
 
     $("#snmt3 li:nth-of-type(3)").click(function(){
-        $("#snmp3").stop().animate({marginLeft: -549});
+        $("#snmp3").stop().animate({marginLeft: -chars * 2});
     });
     
 });
